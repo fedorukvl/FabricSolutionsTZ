9104652c70852a8e19dc95d55039732489c1a1f1
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-16 13:25:48 +0300

    Сверстан компонент навигации

---
+++ components/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navigation">
+        <ul class="navigation__list">
+            <li class="navigation__link navigation__link_logo"></li>
+            <li class="navigation__link">
+                <!--<svg> Добавить иконки-->
+                <p class="navigation__link_title">Опросы</p>
+            </li>
+            <li class="navigation__link">
+                <!--<svg> Добавить иконки-->
+                <p class="navigation__link_title">Пользователи</p>
+            </li>
+            <li class="navigation__link">
+                <!--<svg> Добавить иконки-->
+                <p class="navigation__link_title">Черные списки</p>
+            </li>
+            <li class="navigation__link">
+                <!--<svg> Добавить иконки-->
+                <p class="navigation__link_title">Колл-центр</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
