a7556fef4856bf5c465d17ff9425919d94a8246b
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-16 16:55:38 +0300

    Создан вспомогательный компонент иконки

---
+++ components/VIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <img 
+    :class="className"
+    :src="require(`@/media/icons/${name}.svg`)" />
+</template>
+
+<script>
+
+export default {
+  name: 'VIcon',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+    },
+    computed: {
+        className() {
+            return `icon icon_${this.name}`;
+        }
+    }
+}
+</script>
+<style lang="sass" scoped src="@/media/sass/components/VIcon.sass"></style>
