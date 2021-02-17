163b48faaacb536dfb0b10dd7193acf61269cd8d
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-17 16:59:12 +0300

    Импортирован комп.иконки в header

--- components/Header.vue
+++ components/Header.vue
@@ -3,12 +3,12 @@
       <div class="header__info">
           <form class="header__search-form">
               <input 
-                type="text" 
-                class="header__search-form_search"
-                placeholder="Поиск по системе"
+              type="text" 
+              class="header__search-form_search"
+              placeholder="Поиск по системе"
               >
               <button class="header__search-from_button">
-                  
+                  <v-icon name='search'/>
               </button>
           </form>
           <div class="header__user-info">
@@ -18,14 +18,17 @@
           </div>
       </div>
       <div class="header__route-links">
-        
+        <v-icon name='home'/>
+        <v-icon name='forward-arrow'/>
+        <!-- имя роута -->
       </div>
   </header>
 </template>
 
 <script>
-
+import VIcon from './VIcon.vue'
 export default {
+  components: { VIcon },
     name: 'Header', //Знаю, что лучше не называть таким именем компонент, но, к сожалению, ничего лучше не придумал
 }
 </script>
