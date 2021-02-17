8345a0f7e45ed37c1e07b041cf44caa346c82c67
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-17 16:54:27 +0300

    Сверстан компонент header'a

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template> 
+  <header class="header">
+      <div class="header__info">
+          <form class="header__search-form">
+              <input 
+                type="text" 
+                class="header__search-form_search"
+                placeholder="Поиск по системе"
+              >
+              <button class="header__search-from_button">
+                  
+              </button>
+          </form>
+          <div class="header__user-info">
+              <div class="header__user-info-icon"></div>
+              <h3 class="header__user-info_name">Alexander C.</h3>
+              <p class="header__user-info_grade">Администратор</p>
+          </div>
+      </div>
+      <div class="header__route-links">
+        
+      </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+    name: 'Header', //Знаю, что лучше не называть таким именем компонент, но, к сожалению, ничего лучше не придумал
+}
+</script>
+
+<style>
+
+</style>
