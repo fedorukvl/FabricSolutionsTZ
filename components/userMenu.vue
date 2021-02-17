443360bac98b1a9a8088b2691d0004b5a369fc2f
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-17 19:30:01 +0300

    Добавлены стили к компоненту UserInfo

--- components/userMenu.vue
+++ components/userMenu.vue
@@ -7,14 +7,16 @@
               class="header__search-form_search"
               placeholder="Поиск по системе"
               >
-              <button class="header__search-from_button">
+              <button class="header__search-form_button">
                   <v-icon name='search'/>
               </button>
           </form>
           <div class="header__user-info">
-              <div class="header__user-info-icon"></div>
-              <h3 class="header__user-info_name">Alexander C.</h3>
-              <p class="header__user-info_grade">Администратор</p>
+              <div class="header__user-info_icon"></div>
+              <div class="header__user-info_preview">
+                <h3 class="header__user-info_name">Alexander C.</h3>
+                <p class="header__user-info_grade">Администратор</p>
+              </div>
           </div>
       </div>
       <div class="header__route-links">
@@ -33,6 +35,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="sass" src="@/media/sass/components/UserMenu.sass">
 
 </style>
