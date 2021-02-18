958690b918133e64c32bb559d994c6473c2de9ad
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-18 12:51:10 +0300

    Сверстан компонент userSections

---
+++ components/UserSections.vue
@@ -0,0 +1,34 @@
+<template>
+    <main>
+        <ul class="user-sections">
+            <li class="user-sections__section">
+                <button class="user-sections__button">Параметры</button>
+            </li>
+            <li class="user-sections__section">
+                <button class="user-sections__button">Вопросы</button>
+            </li>
+            <li class="user-sections__section">
+                <button class="user-sections__button">Логика</button>
+            </li>
+            <li class="user-sections__section">
+                <button class="user-sections__button">Условия</button>
+            </li>
+            <li class="user-sections__section">
+                <button class="user-sections__button">Респонденты</button>
+            </li>
+        </ul>
+        <keep-alive>
+            <component :is="neededComp"><!--при выборе раздела будет рендерится необходимый компонент --></component>
+        </keep-alive>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
