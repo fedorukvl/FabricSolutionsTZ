78a808aeb820f60f8d0361535ca708353c598e08
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-18 11:06:17 +0300

    Компонент UserMenu добавлен в лэйаут

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div class="layout">
     <navigation/>
     <div class="layout__main">
+      <user-menu/>
       <Nuxt />
     </div>
   </div>
 </template>
 <script>
 import Navigation from '../components/Navigation.vue'
+import UserMenu from '../components/userMenu.vue'
 export default {
   components:{
     Navigation,
+    UserMenu,
   }
 }
 </script>
