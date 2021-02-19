84b85393c37f6c4b6c5e36742ff30d2e35301887
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-19 15:52:03 +0300

    Создана структура условий для рендера

--- components/sections/Respondents.vue
+++ components/sections/Respondents.vue
@@ -1,15 +1,61 @@
 <template>
   <div class="respondents">
       <h1 class="respondents__title">Добавить опрос</h1>
-      <!-- <div 
+      <div 
         v-for="respond in responds"
         :key="respond.id"
         class="respondents__list"
         >
+        <div class="respondents__condition-name">
+            <h3>Условие {{respond.id}}</h3>
+            <select>
+              <option 
+                v-for="option in respond.options"
+                :key="option.id"
+                selected>{{option.title}}</option>
+            </select>
+        </div>
+        <div class="respondents__condition-form">
+          <div v-if="respond.isAge" class="respondents__age-form">
+            <p>{{respond.name}}</p>
+            <label for="from">от</label>
+            <input type="text" id="from"/>
+            <label for="to">до</label>
+            <input type="text" id="to"/>
+          </div>
+          <div v-else-if="respond.isLoyaltyCartType" class="respondents__cart-type-form">
+            <p>{{respond.name}}</p>
+            <select>
+              <option 
+                v-for="cartType in respond.cartTypes"
+                :key="cartType.id"
+                selected
+              >{{cartType.type}}</option>
+            </select>
+          </div>
+          <div v-else-if="respond.isLoyaltyCartStatus" class="respondents__cart-status-form">
+            <p>{{respond.name}}</p>
+            <select>
+              <option 
+                v-for="cartStatus in respond.cartStatus"
+                :key="cartStatus.id"
+                selected
+              >{{cartStatus.status}}</option>
+            </select>
+          </div>
+          <div class="respondints__condition-buttons">
+            <button>
+              <span>+ Добавить {{respond.name}}</span>
+            </button>
+            <button>
+              <span>Удалить условие</span>
+              <v-icon name="basket"/>
+            </button>
+          </div>
+        </div>
       </div>
-       -->
       <div class="respondents__add-condition">
-          <div class="respondents__add-condition_info">
+          <div class="respondents__add-condition_info" @click="addRespond">
             <p class="respondents__add-condition_plus">+</p>
             <p class="respondents__add-condition_description">Нажмите, чтобы добавить условие выборки.</p>
             <p class="respondents__add-condition_description">Все условия связываются собой логическим "И"</p>    
@@ -23,8 +69,64 @@
 </template>
 
 <script>
+import VIcon from '../VIcon.vue';
 export default {
-
+  name:'Respondents',
+  component: {VIcon},
+  data(){
+    return{
+      responds:[{
+        id: 1,
+        name: 'Диапозон',
+        isAge: true,
+        options:[
+          {
+            id:1,
+            title: 'Возраст респондента',
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Тип',
+        isLoyaltyCartType: true,
+        cartTypes:[
+          {
+            id: 1,
+            type: 'Gold',
+          }
+        ],
+        options:[
+          {
+            id:1,
+            title: 'Тип карты лояльности',
+          }
+        ]
+      },
+      {
+        id: 3,
+        name: 'Статус',
+        isLoyaltyCartStatus: true,
+        cartStatus:[
+          {
+            id: 1,
+            status: 'Активна',
+          }
+        ],
+        options:[
+          {
+            id:1,
+            title: 'Статус карты лояльности',
+          }
+        ]
+      }],
+    };
+  },
+  methods:{
+    addRespond(){
+      console.log('test');
+    }
+  }
 }
 </script>
 
