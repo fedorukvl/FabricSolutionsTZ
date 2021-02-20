05a07d821e750f93aeb81101f2a259f2dfb46c44
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-20 22:59:02 +0300

    Обновлена логика добавления/удаления условий и типов

--- components/sections/Conditions.vue
+++ components/sections/Conditions.vue
@@ -4,7 +4,7 @@
         <div class="respondents__condition-name">
             <h3 
             :style="{'color':respond.style.color}"
-            >Условие {{index+1}}</h3>
+            >Условие {{respond.id}}</h3>
             <select class="respondents__condition-select">
               <option 
                 v-for="option in respond.options"
@@ -13,40 +13,44 @@
             </select>
         </div>
         <div class="respondents__condition-form">
-          <div v-if="respond.isAge" class="respondents__age-form">
-            <p>{{respond.name}}</p>
-            <div class="respondents__diapozons">
-              <label for="from">от</label>
-              <input type="text" id="from"/>
-              <label for="to">до</label>
-              <input type="text" id="to"/>
-            </div>
+          <div 
+            v-for="(diapazon,index) in respond.diapazons"
+            :key="index" 
+            class="respondents__age-form">
+                <p>{{respond.name + diapazon.id}}</p>
+                <div class="respondents__diapozons">
+                <label for="from">от</label>
+                <input type="text" id="from" :value="diapazon.from"/>
+                <label for="to">до</label>
+                <input type="text" id="to" :value="diapazon.to"/>
+                </div>
           </div>
-          <div v-else-if="respond.isLoyaltyCartType" class="respondents__cart-type-form">
-            <p>{{respond.name}}</p>
-            <select class="respondents__cart-type-select">
-              <option 
-                v-for="cartType in respond.cartTypes"
-                :key="cartType.id"
-                selected
-              >{{cartType.type}}</option>
-            </select>
+          <div 
+            v-for="(cartType,index) in respond.cartTypes"
+            :key="index" 
+            class="respondents__cart-type-form">
+                <p>{{respond.name + cartType.id}}</p>
+                <select class="respondents__cart-type-select">
+                <option selected>{{cartType.type}}</option>
+                </select>
           </div>
-          <div v-else-if="respond.isLoyaltyCartStatus" class="respondents__cart-status-form">
-            <p>{{respond.name}}</p>
-            <select class="respondents__cart-status-select">
-              <option 
-                v-for="cartStatus in respond.cartStatus"
-                :key="cartStatus.id"
-                selected
-              >{{cartStatus.status}}</option>
-            </select>
+          <div 
+            v-for="(cartStatus,index) in respond.cartStatus"
+            :key="index" 
+            class="respondents__cart-status-form">
+                <p>{{respond.name + cartStatus.id}}</p>
+                <select class="respondents__cart-status-select">
+                <option selected>{{cartStatus.status}}</option>
+                </select>
           </div>
           <div class="respondents__condition-buttons">
-            <button class="respondents__add-button">
+            <button 
+                class="respondents__add-button"
+                @click="addCondition(index,respond.type)"
+            >
               <span>+ Добавить {{respond.name}}</span>
             </button>
-            <button @click="removeCondition(index)"
+            <button @click="removeCondition(index,respond.type)"
                 class="respondents__delete-button">
               <v-icon name="basket"/>
               <span>Удалить условие</span>
@@ -71,10 +75,13 @@ export default {
         }
     },
     methods:{
-        removeCondition(index){
-            this.$emit('delete',index);
-        }
-    }
+        removeCondition(index,type){
+            this.$emit('delete',index,type);
+        },
+        addCondition(index,type){
+            this.$emit('add',index,type)
+        },
+    },
 }
 </script>
 
--- components/sections/Respondents.vue
+++ components/sections/Respondents.vue
@@ -9,6 +9,7 @@
         :respond="respond"
         :index="index"
         @delete="removeCondition"
+        @add="addCondition"
         />
       <div class="respondents__add-condition">
           <div class="respondents__add-condition_info" @click="addRespond">
@@ -25,6 +26,9 @@
 </template>
 
 <script>
+let lastDiapazonId = 1;
+let lastCartStatusId = 1;
+let lastCartTypeId = 1;
 import VIcon from '../VIcon.vue';
 import Conditions from './Conditions.vue';
 export default {
@@ -40,7 +44,7 @@ export default {
     {
         id: 1,
         name: 'Диапозон',
-        isAge: true,
+        type: 'diapazons',
         style: {
           backgroundColor: '#fffcf5',
           color: '#a97a07'
@@ -50,12 +54,19 @@ export default {
             id:1,
             title: 'Возраст респондента',
           }
+        ],
+        diapazons:[
+          {
+            id: 1,
+            from: null,
+            to: null,
+          }
         ]
       },
       {
         id: 2,
         name: 'Тип',
-        isLoyaltyCartType: true,
+        type:'cartTypes',
         style: {
           backgroundColor: '#f8faff',
           color: '#214aa9'
@@ -76,7 +87,7 @@ export default {
       {
         id: 3,
         name: 'Статус',
-        isLoyaltyCartStatus: true,
+        type: 'cartStatus',
         style: {
           backgroundColor: '#fafff8',
           color: '#30a525'
@@ -107,8 +118,53 @@ export default {
         this.pickedConditionsCount=0;
       }
     },
-    removeCondition(index){
+    removeCondition(index,type){
+      lastDiapazonId = 1;
+      lastCartStatusId = 1;
+      lastCartTypeId = 1;
+      this.responds[index][type].splice(1);
       this.responds.splice(index,1);
+    },
+    generateId(value){ // Добавил генерацию ID для диапазона/статуса/типа карты
+      switch(value){
+        case 'diapazons':
+          return lastDiapazonId++;
+          break;
+        case 'cartTypes':
+          return lastCartTypeId++;
+          break;
+        case 'cartStatus':
+          return lastCartStatusId++;
+          break;
+        default:
+          return false;
+      }
+    },
+    addCondition(index,type){
+      this.generateId(type)
+      switch(type){
+        case 'diapazons':
+          this.responds[index][type].push({
+            id: lastDiapazonId ,
+            from: null,
+            to: null,
+          });
+          break;
+        case 'cartTypes': 
+          this.responds[index][type].push({
+            id: lastCartTypeId,
+            type: 'Gold',
+          });
+          break;
+        case 'cartStatus':
+          this.responds[index][type].push({
+            id: lastCartStatusId,
+            status: 'Активна',
+          });
+          break;
+        default:
+          return false;
+      };
     }
   }
 }
